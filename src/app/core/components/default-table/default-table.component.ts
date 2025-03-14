31f88ce6ef5a46952b7d81334ab89e7cc841a4ad
Luis Henrique <luissecundes@gmail.com>
2025-03-14 16:26:38 -0300

    add default table

---
+++ src/app/core/components/default-table/default-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default-table',
+  imports: [],
+  templateUrl: './default-table.component.html',
+  styleUrl: './default-table.component.scss'
+})
+export class DefaultTableComponent {
+
+}
