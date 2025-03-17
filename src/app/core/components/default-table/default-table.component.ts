d5c36d597eb25f7261d653590e5f307ee314439b
Luis Henrique <luissecundes@gmail.com>
2025-03-17 16:11:51 -0300

    add table component

--- src/app/core/components/default-table/default-table.component.ts
+++ src/app/core/components/default-table/default-table.component.ts
@@ -1,11 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-default-table',
-  imports: [],
   templateUrl: './default-table.component.html',
-  styleUrl: './default-table.component.scss'
+  styleUrls: ['./default-table.component.scss'],
+  standalone: true,
+  imports: [CommonModule],
 })
 export class DefaultTableComponent {
+  @Input() columns: { key: string; label: string }[] = [];
+  @Input() data: any[] = [];
+  @Input() actions: {
+    icon: string;
+    tooltip: string;
+    action: (item: any) => void;
+  }[] = [];
+  @Input() showSelectionColumn: boolean = true;
 
+  selectedItems: Set<any> = new Set();
+
+  @Output() selectionChanged: EventEmitter<any[]> = new EventEmitter();
+
+  toggleSelectAll(event: any) {
+    if (event.target.checked) {
+      this.data.forEach((item) => this.toggleSelectItem(item));
+    } else {
+      this.selectedItems.clear();
+    }
+    this.emitSelection();
+  }
+
+  toggleSelectItem(item: any) {
+    if (this.selectedItems.has(item)) {
+      this.selectedItems.delete(item);
+    } else {
+      this.selectedItems.add(item);
+    }
+    this.emitSelection();
+  }
+
+  isAllSelected(): boolean {
+    return this.selectedItems.size === this.data.length;
+  }
+
+  isSelected(item: any): boolean {
+    return this.selectedItems.has(item);
+  }
+
+  emitSelection() {
+    this.selectionChanged.emit(Array.from(this.selectedItems));
+  }
+
+  getSelectedItems(): any[] {
+    return Array.from(this.selectedItems);
+  }
 }
