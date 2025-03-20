65f0b64f6993c720ddcf489641f47ab0ceb06b68
Luis Henrique <luissecundes@gmail.com>
2025-03-20 16:40:07 -0300

    add

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HOME_DATA } from './data';
 import { CarouselComponent } from '../../core/components/carousel/carousel.component';
+import { NavbarComponent } from '../../core/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
-  imports: [CarouselComponent],
+  imports: [CarouselComponent, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
