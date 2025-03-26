8a3356d63ae907c35141d1b0a68a759c9c7e2a56
Luis Henrique <luissecundes@gmail.com>
2025-03-26 15:14:31 -0300

    add

--- src/app/core/components/dynamic-table/dynamic-table.component.ts
+++ src/app/core/components/dynamic-table/dynamic-table.component.ts
@@ -18,12 +18,12 @@ export class DynamicTableComponent {
   }[] = [];
   @Input() showSelectionColumn: boolean = true;
 
-  // Estado da seleção
-  selectedItems: Set<any> = new Set();
-
   @Output() selectionChanged: EventEmitter<any[]> = new EventEmitter();
 
-  // Seleciona/deseleciona todos os itens
+  selectedItems: Set<any> = new Set();
+  sortColumn: string = ''; // Coluna para ordenação
+  sortDirection: 'asc' | 'desc' = 'asc'; // Direção da ordenação
+
   toggleSelectAll(event: any) {
     if (event.target.checked) {
       this.data.forEach((item) => this.toggleSelectItem(item));
@@ -33,7 +33,6 @@ export class DynamicTableComponent {
     this.emitSelection();
   }
 
-  // Alterna a seleção de um item individual
   toggleSelectItem(item: any) {
     if (this.selectedItems.has(item)) {
       this.selectedItems.delete(item);
@@ -43,24 +42,56 @@ export class DynamicTableComponent {
     this.emitSelection();
   }
 
-  // Verifica se todos os itens estão selecionados
   isAllSelected(): boolean {
     return this.selectedItems.size === this.data.length;
   }
 
-  // Verifica se um item está selecionado
   isSelected(item: any): boolean {
     return this.selectedItems.has(item);
   }
 
-  // Emite os itens selecionados para o componente pai
   emitSelection() {
     this.selectionChanged.emit(Array.from(this.selectedItems));
   }
 
-  // Retorna os itens selecionados
-
   getSelectedItems(): any[] {
     return Array.from(this.selectedItems);
+  }
+
+  // Função para ordenar os dados
+  sortData(columnKey: string) {
+    if (this.sortColumn === columnKey) {
+      this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+    } else {
+      this.sortColumn = columnKey;
+      this.sortDirection = 'asc';
+    }
+
+    this.data.sort((a, b) => {
+      const valA = a[columnKey];
+      const valB = b[columnKey];
+
+      if (valA < valB) {
+        return this.sortDirection === 'asc' ? -1 : 1;
+      } else if (valA > valB) {
+        return this.sortDirection === 'asc' ? 1 : -1;
+      }
+      return 0;
+    });
+  }
+
+  // Método para verificar se a coluna está ordenada
+  isSorted(columnKey: string): boolean {
+    return this.sortColumn === columnKey;
+  }
+
+  // Método para verificar a direção da ordenação
+  getSortDirection(columnKey: string): string {
+    return this.sortColumn === columnKey ? this.sortDirection : '';
+  }
+
+
+  trackItem(index: number, item: any): any {
+    return item ? item.id || item : index;
   }
 }
