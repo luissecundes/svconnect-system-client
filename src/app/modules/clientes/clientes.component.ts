cf99a03d2d02f9ffb9963b5dcb52cbff5df8bada
Luis Henrique <luissecundes@gmail.com>
2025-03-26 12:09:50 -0300

    add

--- src/app/modules/clientes/clientes.component.ts
+++ src/app/modules/clientes/clientes.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../core/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-clientes',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './clientes.component.html',
   styleUrl: './clientes.component.scss'
 })
