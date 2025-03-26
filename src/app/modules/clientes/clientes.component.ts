72389a419f7608d47aaa13b31729acbd4cdb6997
Luis Henrique <luissecundes@gmail.com>
2025-03-26 15:23:54 -0300

    subindo

--- src/app/modules/clientes/clientes.component.ts
+++ src/app/modules/clientes/clientes.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../core/components/navbar/navbar.component';
 import { MenuComponent } from '../../core/components/menu/menu.component';
+import { TestDynamicTableComponent } from '../../test-dynamic-table/test-dynamic-table.component';
 
 @Component({
   selector: 'app-clientes',
-  imports: [NavbarComponent, MenuComponent],
+  imports: [NavbarComponent, MenuComponent, TestDynamicTableComponent],
   templateUrl: './clientes.component.html',
   styleUrl: './clientes.component.scss'
 })
