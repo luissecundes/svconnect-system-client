43e3d61082cefbcdfa8ea3d05098d8a41c688b8d
Luis Henrique <luissecundes@gmail.com>
2025-03-26 12:02:08 -0300

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,14 @@
-import { Component, ChangeDetectorRef } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './modules/auth/login.component';
 import { NavbarComponent } from './core/components/navbar/navbar.component';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'original-portas-client';
-  showNavbar = true;
-
-  constructor(private router: Router, private cdr: ChangeDetectorRef) {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        console.log('URL Atual:', event.url);
-        this.showNavbar = !event.url.startsWith('/login');
-        console.log('Mostrar Navbar?', this.showNavbar);
-        this.cdr.detectChanges(); // Força a atualização do Angular
-      }
-    });
-  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './modules/auth/login.component';
 import { HomeComponent } from './modules/home/home.component';
+import { ClientesComponent } from './modules/clientes/clientes.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
+  { path: 'clientes', component: ClientesComponent },
 
     // generic 404
     { path: "**", redirectTo: "home" },
