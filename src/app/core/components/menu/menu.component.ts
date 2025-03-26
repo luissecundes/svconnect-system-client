b79b831c29394d15f8e21a32191b1e51e793a977
Luis Henrique <luissecundes@gmail.com>
2025-03-26 13:28:08 -0300

    add options

--- src/app/core/components/menu/menu.component.ts
+++ src/app/core/components/menu/menu.component.ts
@@ -4,8 +4,31 @@ import { Component } from '@angular/core';
   selector: 'app-menu',
   imports: [],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+  styleUrl: './menu.component.scss',
 })
 export class MenuComponent {
+  onInsert() {
+    console.log('Inserir clicado');
+    // Lógica para inserção
+  }
 
+  onEdit() {
+    console.log('Editar clicado');
+    // Lógica para edição
+  }
+
+  onOption1() {
+    console.log('Opção 1 clicada');
+    // Lógica para a opção 1
+  }
+
+  onOption2() {
+    console.log('Opção 2 clicada');
+    // Lógica para a opção 2
+  }
+
+  onOption3() {
+    console.log('Opção 3 clicada');
+    // Lógica para a opção 3
+  }
 }
--- src/app/modules/clientes/clientes.component.ts
+++ src/app/modules/clientes/clientes.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../core/components/navbar/navbar.component';
+import { MenuComponent } from '../../core/components/menu/menu.component';
 
 @Component({
   selector: 'app-clientes',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, MenuComponent],
   templateUrl: './clientes.component.html',
   styleUrl: './clientes.component.scss'
 })
