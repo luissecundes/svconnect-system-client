43ec333e3bb1689415240fe61f51e41c93f7e823
Luis Henrique <luissecundes@gmail.com>
2025-03-27 01:19:31 -0300

    add

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { HOME_DATA } from './data';
-import { TestDynamicTableComponent } from '../../test-dynamic-table/test-dynamic-table.component';
 import { SidenavComponent } from '../../core/components/sidenav/sidenav.component';
 import { CommonModule } from '@angular/common';
 import { CarouselComponent } from '../../core/components/carousel/carousel.component';
+import { NavbarComponent } from '../../core/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-home',
-  imports: [TestDynamicTableComponent, SidenavComponent, CommonModule, CarouselComponent],
+  imports: [NavbarComponent, SidenavComponent, CommonModule, CarouselComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
