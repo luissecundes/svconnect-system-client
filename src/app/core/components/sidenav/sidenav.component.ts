a1c4f342a52437975360081b0dd0fb4fb5a6f362
Luis Henrique <luissecundes@gmail.com>
2025-03-27 00:54:39 -0300

    add sidebar responsiva

---
+++ src/app/core/components/sidenav/sidenav.component.ts
@@ -0,0 +1,42 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { navbarData } from './nav-data';
+import { RouterModule } from '@angular/router';
+
+interface SideNavToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
+
+@Component({
+  selector: 'app-sidenav',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './sidenav.component.html',
+  styleUrl: './sidenav.component.scss',
+})
+
+export class SidenavComponent implements OnInit{
+  @Output() onToggleSidenav: EventEmitter<SideNavToggle> = new EventEmitter();
+  collapsed = false;
+  screenWidth = 0;
+  navData = navbarData;
+
+  ngOnInit(): void {
+    this.screenWidth = window.innerWidth;
+  }
+
+  toggleCollapsed(): void {
+    this.collapsed = !this.collapsed;
+    this.onToggleSidenav.emit({
+      collapsed: this.collapsed,
+      screenWidth: this.screenWidth,
+    });
+  }
+  closeSidenav(): void {
+    this.collapsed = false;
+    this.onToggleSidenav.emit({
+      collapsed: this.collapsed,
+      screenWidth: this.screenWidth,
+    });
+  }
+}
--- src/app/modules/clientes/clientes.component.ts
+++ src/app/modules/clientes/clientes.component.ts
@@ -2,13 +2,25 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../../core/components/navbar/navbar.component';
 import { MenuComponent } from '../../core/components/menu/menu.component';
 import { TestDynamicTableComponent } from '../../test-dynamic-table/test-dynamic-table.component';
+import { SidenavComponent } from '../../core/components/sidenav/sidenav.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-clientes',
-  imports: [NavbarComponent, MenuComponent, TestDynamicTableComponent],
+  imports: [
+    NavbarComponent,
+    MenuComponent,
+    TestDynamicTableComponent,
+    SidenavComponent,
+    CommonModule
+  ],
   templateUrl: './clientes.component.html',
-  styleUrl: './clientes.component.scss'
+  styleUrl: './clientes.component.scss',
 })
 export class ClientesComponent {
+  sidenavCollapsed = false;
 
+  handleSidenavToggle(event: { collapsed: boolean; screenWidth: number }) {
+    this.sidenavCollapsed = event.collapsed;
+  }
 }
