3338858d2c18450fddd8504bda88d9a03b0b7787
Luis Henrique <luissecundes@gmail.com>
2025-03-29 16:38:03 -0300

    criação de módulo de estoque, financeiro, orçamentos e vendas

---
+++ src/app/modules/orcamentos/orcamentos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orcamentos',
+  imports: [],
+  templateUrl: './orcamentos.component.html',
+  styleUrl: './orcamentos.component.scss'
+})
+export class OrcamentosComponent {
+
+}
