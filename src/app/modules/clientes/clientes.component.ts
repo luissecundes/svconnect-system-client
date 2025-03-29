7c31589eb6ec1102835c5bad16ba4e9ce01ea2b3
Luis Henrique <luissecundes@gmail.com>
2025-03-29 14:25:01 -0300

    add

--- src/app/modules/clientes/clientes.component.ts
+++ src/app/modules/clientes/clientes.component.ts
@@ -2,12 +2,18 @@ import { Component } from '@angular/core';
 import { TestDynamicTableComponent } from '../../test-dynamic-table/test-dynamic-table.component';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from '../../core/components/layout/layout.component';
+import { SidenavComponent } from '../../core/components/sidenav/sidenav.component';
+import { NavbarComponent } from '../../core/components/navbar/navbar.component';
+import { CarouselComponent } from '../../core/components/carousel/carousel.component';
 
 @Component({
   selector: 'app-clientes',
   imports: [
     TestDynamicTableComponent,
     LayoutComponent,
+    SidenavComponent,
+    NavbarComponent,
+    CarouselComponent,
     CommonModule
   ],
   templateUrl: './clientes.component.html',
