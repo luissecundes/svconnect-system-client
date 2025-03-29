58e6527440e0dc28abc07190a1f64f84bd8a6558
Luis Henrique <luissecundes@gmail.com>
2025-03-29 11:34:33 -0300

    criada rota para clientes e opção de menus com dropdown

--- src/app/core/components/sidenav/nav-data.ts
+++ src/app/core/components/sidenav/nav-data.ts
@@ -1,7 +1,17 @@
 export const navbarData = [
   {
-    routeLink: 'dashboard',
+    routeLink: '/home',
     icon: 'fal fa-home',
     label: 'Home',
-  }
-]
+  },
+  {
+    routeLink: '/clientes',
+    icon: 'fal fa-users',
+    label: 'Clientes',
+    // expanded: false, // <-- propriedade de dropdown
+    // submenu: [
+    //   { routeLink: '/clientes/cadastrar', label: 'Cadastrar' },
+    //   { routeLink: '/clientes/editar', label: 'Editar' },
+    // ],
+  },
+];
