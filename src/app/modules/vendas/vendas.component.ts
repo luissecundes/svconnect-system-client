1c27b916a2438f46caf54fc1280f9120125d50c9
Luis Henrique <luissecundes@gmail.com>
2025-03-29 18:17:49 -0300

    criação da tela de vendas

--- src/app/modules/vendas/vendas.component.ts
+++ src/app/modules/vendas/vendas.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { SidenavComponent } from '../../core/components/sidenav/sidenav.component';
+import { NavbarComponent } from '../../core/components/navbar/navbar.component';
+import { MenuComponent } from '../../core/components/menu/menu.component';
+import { CommonModule } from '@angular/common';
+import { SidenavBaseComponent } from '../../core/utils/sidenav.mixin';
 
 @Component({
   selector: 'app-vendas',
-  imports: [],
+  imports: [SidenavComponent, NavbarComponent, MenuComponent, CommonModule],
   templateUrl: './vendas.component.html',
-  styleUrl: './vendas.component.scss'
+  styleUrl: './vendas.component.scss',
 })
-export class VendasComponent {
-
-}
+export class VendasComponent extends SidenavBaseComponent {}
