5290e8deff78b22a75186c5ac703f84b2d1dc5bc
Luis Henrique <luissecundes@gmail.com>
2025-03-29 16:25:52 -0300

    add

--- src/app/core/components/sidenav/nav-data.ts
+++ src/app/core/components/sidenav/nav-data.ts
@@ -1,12 +1,37 @@
 export const navbarData = [
   {
     routeLink: '/home',
-    icon: 'fal fa-home',
+    icon: 'fas fa-home',
     label: 'Home',
   },
   {
     routeLink: '/clientes',
-    icon: 'fas fa-clipboard-user',
+    icon: 'fa fa-users',
     label: 'Clientes',
+  },
+  {
+    routeLink: '/estoque',
+    icon: 'fas fa-boxes',
+    label: 'Estoque',
+  },
+  {
+    routeLink: '/financeiro',
+    icon: 'fas fa-dollar-sign',
+    label: 'Financeiro',
+  },
+  {
+    routeLink: '/pcp',
+    icon: 'fas fa-cogs',
+    label: 'PCP',
+  },
+  {
+    routeLink: '/recursos-humanos',
+    icon: 'fas fa-user-tie',
+    label: 'Recursos Humanos',
+  },
+  {
+    routeLink: '/vendas',
+    icon: 'fas fa-shopping-cart',
+    label: 'Vendas',
   },
 ];
