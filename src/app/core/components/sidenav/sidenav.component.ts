71290b74f400b74546891532387d1cf5a39c0fbf
Luis Henrique <luissecundes@gmail.com>
2025-03-29 17:43:33 -0300

    subindo

--- src/app/core/components/sidenav/sidenav.component.ts
+++ src/app/core/components/sidenav/sidenav.component.ts
@@ -2,23 +2,21 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { navbarData } from './nav-data';
 import { Router, RouterModule } from '@angular/router';
-
-interface SideNavToggle {
-  screenWidth: number;
-  collapsed: boolean;
-}
+import { SideNavToggle } from '../../interfaces/sidenav-toggle.interface';
 
 @Component({
   selector: 'app-sidenav',
+  standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './sidenav.component.html',
-  styleUrl: './sidenav.component.scss',
+  styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent implements OnInit {
   @Output() onToggleSidenav: EventEmitter<SideNavToggle> = new EventEmitter();
   collapsed = false;
   screenWidth = 0;
   navData = navbarData;
+
   constructor(private router: Router) {}
 
   navigateTo(route: string) {
@@ -36,6 +34,7 @@ export class SidenavComponent implements OnInit {
       screenWidth: this.screenWidth,
     });
   }
+
   closeSidenav(): void {
     this.collapsed = false;
     this.onToggleSidenav.emit({
