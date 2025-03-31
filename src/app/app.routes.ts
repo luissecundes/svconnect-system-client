fd09c707e69036618e9f714197e4570438203f4c
Luis Henrique <luissecundes@gmail.com>
2025-03-31 08:42:56 -0300

    alteração na estrutura de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,31 +2,62 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './modules/auth/login.component';
 import { HomeComponent } from './modules/home/home.component';
 import { ClientesComponent } from './modules/clientes/clientes.component';
+import { AuthGuard } from './guards/auth.guard'; // Import do guard
 import { EstoqueComponent } from './modules/estoque/estoque.component';
 import { FinanceiroComponent } from './modules/financeiro/financeiro.component';
+import { PcpComponent } from './modules/pcp/pcp.component';
+import { RecursosHumanosComponent } from './modules/recursos-humanos/recursos-humanos.component';
 import { VendasComponent } from './modules/vendas/vendas.component';
-import { AuthGuard } from './guards/auth.guard';
 
-// Mapeamento de rotas para componentes
-const routeComponents: { [key: string]: any } = {
-  home: HomeComponent,
-  clientes: ClientesComponent,
-  estoque: EstoqueComponent,
-  financeiro: FinanceiroComponent,
-  vendas: VendasComponent,
-};
-
-// Configuração de rotas
+// Colocar o AuthGuard em todas as rotas quando o sistema estiver pronto
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
 
-  // Criando rotas dinamicamente a partir do objeto
-  ...Object.keys(routeComponents).map((path) => ({
-    path,
-    component: routeComponents[path],
-    // canActivate: [AuthGuard], // Protegendo todas as rotas com AuthGuard
-  })),
+  //HomeComponent
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+
+  // ClientesComponent
+  {
+    path: 'clientes',
+    component: ClientesComponent,
+  },
+
+  // EstoqueComponent
+  {
+    path: 'estoque',
+    component: EstoqueComponent,
+  },
+
+  // FinanceiroComponent
+  {
+    path: 'financeiro',
+    component: FinanceiroComponent,
+  },
+
+  // PcpComponent
+  {
+    path: 'pcp',
+    component: PcpComponent,
+  },
+
+  // RecursosHumanosComponent
+  {
+    path: 'recursos-humanos',
+    component: RecursosHumanosComponent,
+  },
+
+  // VendasComponent
+  {
+    path: 'vendas',
+    component: VendasComponent,
+  },
 
   { path: '**', redirectTo: 'home' },
 ];
