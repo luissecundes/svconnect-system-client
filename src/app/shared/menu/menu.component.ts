835fdcd232f44dd699e7bc8c719130e3716ecb1c
Luis Henrique <luissecundes@gmail.com>
2025-04-14 19:46:56 -0300

    animations

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,3 +1,4 @@
+import { trigger, transition, style, animate } from '@angular/animations';
 import { Component, Output, EventEmitter, OnDestroy } from '@angular/core';
 import { SelectionService } from '../../services/selection/selection.service';
 import { CommonModule } from '@angular/common';
@@ -9,6 +10,23 @@ import { Subscription } from 'rxjs';
   imports: [CommonModule],
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
+  animations: [
+    trigger('flipIn', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'rotateY(-90deg)' }),
+        animate(
+          '300ms ease-out',
+          style({ opacity: 1, transform: 'rotateY(0)' })
+        ),
+      ]),
+      transition(':leave', [
+        animate(
+          '200ms ease-in',
+          style({ opacity: 0, transform: 'rotateY(90deg)' })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class MenuComponent implements OnDestroy {
   @Output() insert = new EventEmitter<void>();
@@ -18,6 +36,8 @@ export class MenuComponent implements OnDestroy {
   @Output() export = new EventEmitter<void>();
 
   dropdownOpen = false;
+  hasEdited: boolean = false; // Variável para controlar a visibilidade do botão Alterar
+
   selectedCount: number = 0;
   private routerSubscription: Subscription;
 
@@ -27,6 +47,7 @@ export class MenuComponent implements OnDestroy {
   ) {
     this.selectionService.selectedItems$.subscribe((selectedItems) => {
       this.selectedCount = selectedItems.length;
+      this.hasEdited = this.selectedCount === 1; // Atualiza se o botão Alterar deve ser exibido
     });
 
     this.routerSubscription = this.router.events.subscribe((event) => {
@@ -42,6 +63,7 @@ export class MenuComponent implements OnDestroy {
 
   onEdit() {
     this.edit.emit();
+    this.hasEdited = false; // Após editar, escondemos o botão
   }
 
   onOption1() {
